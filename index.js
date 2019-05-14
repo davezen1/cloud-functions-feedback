6e35c5a43582d6ec44b0face6281e2cc53db3c1c
David Hodge <david.hodge@lucidtechnics.com>
2019-05-14 08:31:41 -0400

    chore: Initial add and setup of CI/CD
    - using nodejs10
    - Cloud Build
    - env file .env.yaml

---
+++ index.js
@@ -0,0 +1,40 @@
+const Firestore = require('@google-cloud/firestore')
+const PROJECTID = 'lucid-universal-services'
+const COLLECTION_NAME = 'feedbacks'
+const firestore = new Firestore({
+  projectId: PROJECTID,
+  timestampsInSnapshots: true
+})
+
+exports.feedback = (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  // res.set('Access-Control-Allow-Origin', 'https://mydomain.com')
+  res.set('Access-Control-Allow-Credentials', 'true')
+
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'GET')
+    res.set('Access-Control-Allow-Headers', 'Content-Type')
+    res.set('Access-Control-Max-Age', '3600')
+    res.status(204).send('')
+  } else if (req.method === 'POST') {
+    // store/insert a new document
+    const data = (req.body) || {}
+    const appId = data.usappid
+    const user = data.user || {}
+    const feedback = data.feedback
+    const created = new Date().getTime()
+    return firestore.collection(COLLECTION_NAME)
+      .add({ created, appId, feedback, user})
+      .then(doc => {
+        return res.status(200).send(doc)
+      }).catch(err => {
+      console.error(err)
+      return res.status(404).send({ error: 'unable to store' + appId + ' ' + feedback, err})
+    })
+  } else if (req.method === 'GET' ||
+             req.method === 'PUT' || 
+             req.method === 'DELETE') { 
+    res.status(405).send('Method Not Implemented')
+  }
+}
