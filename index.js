b8444d98af5054ae53392b215ee560233b5ea213
David Hodge <david.hodge@lucidtechnics.com>
2019-05-24 10:59:11 -0400

    chore: updates CORS and OPTIONS

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const firestore = new Firestore({
 
 exports.feedback = (req, res) => {
   res.set('Access-Control-Allow-Origin', '*')
+  // Perhaps add CORS with Authorization credentials
   // res.set('Access-Control-Allow-Origin', 'https://mydomain.com')
-  res.set('Access-Control-Allow-Credentials', 'true')
+  // res.set('Access-Control-Allow-Credentials', 'true')
 
   if (req.method === 'OPTIONS') {
     // Send response to OPTIONS requests
-    res.set('Access-Control-Allow-Methods', 'GET')
+    res.set('Access-Control-Allow-Methods', 'POST')
     res.set('Access-Control-Allow-Headers', 'Content-Type')
     res.set('Access-Control-Max-Age', '3600')
     res.status(204).send('')
